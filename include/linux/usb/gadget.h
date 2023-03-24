edc4a1941a3a57c25014f0f943b554fe887faf70
Elson Roy Serrao <quic_eserrao@quicinc.com>
2023-03-24 14:47:59 -0700

    usb: gadget: Add function wakeup support
    [ Upstream commit f0db885fb05d35befa81896db6b19eb3ee9ccdfe ]
    
    USB3.2 spec section 9.2.5.4 quotes that a function may signal that
    it wants to exit from Function Suspend by sending a Function
    Wake Notification to the host if it is enabled for function
    remote wakeup. Add an api in composite layer that can be used
    by the function drivers to support this feature. Also expose
    a gadget op so that composite layer can trigger a wakeup request
    to the UDC driver.
    
    Reviewed-by: Thinh Nguyen <Thinh.Nguyen@synopsys.com>
    Signed-off-by: Elson Roy Serrao <quic_eserrao@quicinc.com>
    Link: https://lore.kernel.org/r/1679694482-16430-4-git-send-email-quic_eserrao@quicinc.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Stable-dep-of: 705e3ce37bcc ("usb: dwc3: core: Fix system suspend on TI AM62 platforms")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/linux/usb/gadget.h
+++ include/linux/usb/gadget.h
@@ -317,6 +317,8 @@ struct usb_udc;
 struct usb_gadget_ops {
 	int	(*get_frame)(struct usb_gadget *);
 	int	(*wakeup)(struct usb_gadget *);
+	int	(*func_wakeup)(struct usb_gadget *gadget, int intf_id);
+	int	(*set_remote_wakeup)(struct usb_gadget *, int set);
 	int	(*set_selfpowered) (struct usb_gadget *, int is_selfpowered);
 	int	(*vbus_session) (struct usb_gadget *, int is_active);
 	int	(*vbus_draw) (struct usb_gadget *, unsigned mA);
