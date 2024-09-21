9d1137a249602b56010b1d39d3512d04a81ac334
Chuck Lever <chuck.lever@oracle.com>
2024-09-21 14:25:37 -0400

    NFSD: Mark filecache "down" if init fails
    [ Upstream commit dc0d0f885aa422f621bc1c2124133eff566b0bc8 ]
    
    NeilBrown says:
    > The handling of NFSD_FILE_CACHE_UP is strange.  nfsd_file_cache_init()
    > sets it, but doesn't clear it on failure.  So if nfsd_file_cache_init()
    > fails for some reason, nfsd_file_cache_shutdown() would still try to
    > clean up if it was called.
    
    Reported-by: NeilBrown <neilb@suse.de>
    Fixes: c7b824c3d06c ("NFSD: Replace the "init once" mechanism")
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/nfsd/filecache.c
+++ fs/nfsd/filecache.c
@@ -630,6 +630,11 @@ nfsd_file_cache_init(void)
 	if (nfsd_file_hashtbl)
 		return 0;
 
+	ret = rhltable_init(&nfsd_file_rhltable, &nfsd_file_rhash_params);
+	if (ret)
+		goto out;
+
+	ret = -ENOMEM;
 	nfsd_filecache_wq = alloc_workqueue("nfsd_filecache", 0, 0);
 	if (!nfsd_filecache_wq)
 		goto out;
@@ -690,6 +695,8 @@ nfsd_file_cache_init(void)
 
 	INIT_DELAYED_WORK(&nfsd_filecache_laundrette, nfsd_file_gc_worker);
 out:
+	if (ret)
+		clear_bit(NFSD_FILE_CACHE_UP, &nfsd_file_flags);
 	return ret;
 out_notifier:
 	lease_unregister_notifier(&nfsd_file_lease_notifier);
